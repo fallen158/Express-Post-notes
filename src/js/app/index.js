62672a1b68c9428fe1497a4d271fec6b2107955f
fallen158 <79334424@qq.com>
2018-07-24 10:28:39 +0800

    搭建Express-cli及配置wepback

---
+++ src/js/app/index.js
@@ -0,0 +1,6 @@
+var obj  =require('../mod/b.js')
+
+
+console.log(obj)
+
+console.log('Hello Webpack!!!')
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/app/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './public/javascripts')
+  }
+};
